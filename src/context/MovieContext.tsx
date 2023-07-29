ae25bdf46b5d033a6d50ab637330979c38ff9ead
spapierz <sarah.papierz@gmail.com>
2023-07-28 23:23:50 -0500

    sets up the 2 states we want to track throughout the app aka movies and favorites

--- src/context/MovieContext.tsx
+++ src/context/MovieContext.tsx
@@ -1,5 +1,18 @@
 import React, { createContext, useState, useEffect, ReactNode } from 'react';
 import axios from 'axios';
-import { MovieContextData } from '../interfaces/Movies';
+import { Movie, MovieContextData } from '../interfaces/Movies';
+
+interface MovieContextProviderProps {
+    children: ReactNode;
+}
 
 export const MovieContext = createContext<MovieContextData>({} as MovieContextData);
+
+const MovieContextProvider: React.FC<MovieContextProviderProps> = ({ children }) => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [favorites, setFavorites] = useState<Movie[]>([]);
+    
+    return children;
+};
+
+export default MovieContextProvider;
