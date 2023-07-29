1f5047bdc29751b32e9519d7ba9994bf7a0b2c8b
spapierz <sarah.papierz@gmail.com>
2023-07-28 22:23:48 -0500

    creates movie list cards and mock data

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,40 @@ const LazyMovieList = lazy(() => import('./components/MovieList'));
 const LazySearchBar = lazy(() => import('./components/SearchBar'));
 
 const App: React.FC = () => {
+  // Sample movie data as an array of Movie objects
+  const movies = [
+    {
+      title: 'Barbie',
+      poster: 'barbie-image',
+      description: 'barbie and ken lands',
+      releaseDate: 'July',
+      rating: '5*',
+      imdbID: 'barbie',
+    },
+    {
+      title: 'Indiana Jones',
+      poster: 'indiana-jones-image',
+      description: 'dial of doom',
+      releaseDate: 'june',
+      rating: '5*',
+      imdbID: 'indiana',
+    },
+  ];
+
   return (
     <div className="App">
-      <Container maxWidth="sm">
+      <Container>
         <Typography variant="h2" gutterBottom>
           Movie App
         </Typography>
         <Suspense fallback={<div>Loading...</div>}>
           <LazySearchBar />
-          <LazyMovieList />
+          {/* Pass the movies prop */}
+          <LazyMovieList movies={movies} />
         </Suspense>
       </Container>
     </div>
-  )
+  );
 };
 
 export default App;
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Movie } from '../interfaces/Movies';
+import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
 
-const MovieItem = () => {
+interface MovieListProps {
+    movies: Movie[];
+}
+
+const MovieList: React.FC<MovieListProps> = ({ movies }) => {
     return (
-        <div>
-            test
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            {movies.map((movie) => (
+                <Card key={movie.imdbID} style={{ margin: '10px', minWidth: '300px' }}>
+                    <Typography variant="h5">{movie.title}</Typography>
+                    <CardMedia image={movie.poster} title={movie.title} />
+                    <CardContent>
+                        <Typography variant="body2">{movie.description}</Typography>
+                    </CardContent>
+                </Card>
+            ))}
         </div>
-    )
+    );
 };
 
-export default MovieItem;
+export default MovieList;
