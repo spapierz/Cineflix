5830464cf684c42dfd12430dafe7f08f61b636ac
spapierz <sarah.papierz@gmail.com>
2023-07-28 21:47:12 -0500

    fixes dependency issues

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { Suspense, lazy } from 'react';
+import { Container, Typography } from '@mui/material';
+import './app.scss';
+
+const LazyMovieList = lazy(() => import('./components/MovieList'));
+const LazySearchBar = lazy(() => import('./components/SearchBar'));
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Container maxWidth="sm">
+        <Typography variant="h2" gutterBottom>
+          Movie App
+        </Typography>
+        <Suspense fallback={<div>Loading...</div>}>
+          <LazySearchBar />
+          <LazyMovieList />
+        </Suspense>
+      </Container>
+    </div>
+  )
+};
+
+export default App;
