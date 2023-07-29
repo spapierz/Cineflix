3e91547564e726b0db39fed99c358f5cd0f5c789
spapierz <sarah.papierz@gmail.com>
2023-07-28 20:12:44 -0500

    sets up the App as the container which will lazy load the rest of the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-//import logo from './logo.svg';
-//import './App.css';
+import React, { Suspense, lazy } from 'react';
+import { Container, Typography } from '@mui/material';
+
+const LazyMovieList = lazy(() => import('./components/MovieList'));
+const LazySearchBar = lazy(() => import('./components/SearchBar'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        test
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Typography variant="h2" gutterBottom>
+        Movie App
+      </Typography>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyMovieList />
+        <LazySearchBar />
+      </Suspense>
+    </Container>
   );
 }
 
